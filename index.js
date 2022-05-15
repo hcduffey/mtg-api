ea94cb3a46e5d9868b55c7d895f6c0139309d20b
hcduffey <cliff.duffey@pm.me>
2022-05-14 21:55:00 -0400

    added middleware

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// IMPORTS
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 4000;
 require('./config/db.connect');
+
+// MIDDLEWARE
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
 
 app.get("/", (req, res) => res.send("in root path"));
 app.listen(PORT, () => console.log('Express up on port ' + PORT));
