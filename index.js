10f9ed8d66d602cdbaade8a78bd60986fb5f172c
hcduffey <cliff.duffey@pm.me>
2022-05-14 21:52:09 -0400

    connected to database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+require('./config/db.connect');
 
 app.get("/", (req, res) => res.send("in root path"));
-
 app.listen(PORT, () => console.log('Express up on port ' + PORT));
